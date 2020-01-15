f67db9bc55ace0d8bb2c8b621b6a4a06d22a785a
diskszkdev <diskszkdev@gmail.com>
2020-01-16 00:03:48 +0900

    add new project

---
+++ PrismTemplate/PrismTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrismTemplate.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public ICommand RegistCommand { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            Message = "Hello!!!";
+            RegistCommand = new DelegateCommand(() => Message = "Clicked!!!");
+        }
+    }
+}
